9bf4fd36ae864340c0a1dc788a7e3cdd79ed65c4
k-obata-3 <rimona.obata.3@gmail.com>
2021-08-09 17:24:25 +0900

    同名ファイルをアップロード時に連番を付与するよう実装（上書き更新の簡易対応）

--- fileUploader.php
+++ fileUploader.php
@@ -14,11 +14,13 @@ class FileUploader extends Base
         try
         {
             $tempfile = $_FILES['up-file']['tmp_name'];
-            $filename = UPLOAD_DIR ."/" .$_FILES['up-file']['name'];
+            $fileName = UPLOAD_DIR ."/" .$_FILES['up-file']['name'];
+            // 一意のファイル名を取得
+            $upFileName = $this-> getUniqueFileName($fileName);
 
             if (is_uploaded_file($tempfile))
             {
-                move_uploaded_file($tempfile , $filename );
+                move_uploaded_file($tempfile , $upFileName);
             }
 
             header('Location: fileList.php');
@@ -27,6 +29,26 @@ class FileUploader extends Base
         {
             printf($e-> getMessage());
         }
+    }
+
+    /**
+     * 一意のファイル名を取得
+     * 同名ファイル名が存在する場合は連番を付与
+     */
+    function getUniqueFileName($fileName)
+    {
+        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $baseFileName = pathinfo($fileName, PATHINFO_FILENAME);
+
+        $uniqueFileName = $fileName;
+        $index = 2;
+        while (file_exists($uniqueFileName))
+        {
+            // 連番を付与 [ファイル名]([連番]).[拡張子]
+            $uniqueFileName = UPLOAD_DIR ."/" .$baseFileName .'(' .$index++ .')' .'.' .$extension;
+        }
+
+        return $uniqueFileName;
     }
 }
 new FileUploader();
